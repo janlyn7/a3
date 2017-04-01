5a8ff22237b38e0dbe4cf5acc9130dfc88b7fb6a
Janine Lyn <jlyn@cfa.harvard.edu>
2017-04-01 08:57:58 -0400

    Created BillSplitterController and index view

--- app/Http/Controllers/BillSplitterController.php
+++ app/Http/Controllers/BillSplitterController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class BillSplitterController extends Controller
 {
-    //
+
+    // main routine for displaying Split the Check calculator
+    public function index(Request $request) {
+        dump($request);
+        return view('index');
+    }
+
+
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Assignment 2 - Split the Check
+        </title>
+        <meta charset='utf-8'>
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+        <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' rel='stylesheet' type='text/css' />
+        <link href='https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <h2> Split the Check</h2>
+
+
+    </body>
+</html>
