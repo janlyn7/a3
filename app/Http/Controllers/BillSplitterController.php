6989431fcca90b33a110e4f88f5896ba443c0993
Janine Lyn <jlyn@cfa.harvard.edu>
2017-04-03 07:57:35 -0400

    added comments to BillSplitterController

--- app/Http/Controllers/BillSplitterController.php
+++ app/Http/Controllers/BillSplitterController.php
@@ -7,36 +7,27 @@ use Validator;
 
 class BillSplitterController extends Controller
 {
+
     // main routine for displaying Split the Check calculator
     public function index(Request $request) {
 
+    	// if action == reset, return view with cleared/default field inputs
 	if (($request->has('act') and $request->input('act') == 'reset')) {
-	   $totalBill = 0;
-	   $numPeople = 1;
-	   $tip = 'good';
-	   $roundUp = '';
-
 	   return view('index')->with([
-             'totalBill' => $totalBill,
-             'numPeople' => $numPeople,
-             'tip'       => $tip,
-             'roundUp'   => $roundUp,
+             'totalBill' => 0,
+             'numPeople' => 1,
+             'tip'       => 'good',
+             'roundUp'   => '',
 	   ]);
 	} 
-
-
-        // storage array for how much to tip depending on the quality of service
-        static $serviceType = [                             
-   		    "poor" => 0.10,
-		    "good" => 0.15,
-		    "great"=> 0.20,
-	  	  ];
 
         $ppBill = 0;
         $errmsgs = null;
 
+	// get inputs
         $totalBill = $request->input('totalBill', 0);
         $numPeople = $request->input('numPeople', 1);
+
 	if ($request->has('roundUp'))
 	   $roundUp = 'CHECKED';
 	else
@@ -44,6 +35,7 @@ class BillSplitterController extends Controller
 	$tip = $request->input('tip', 'good');
 
 
+	// validate inputs
 	$rules = array(
        	    'totalBill' => 'required|numeric|min:0|max:10000',
        	    'numPeople' => 'required|integer|min:1|max:20',
@@ -51,11 +43,22 @@ class BillSplitterController extends Controller
 
 	$validator = Validator::make($request->input(), $rules);
 
-        // get the error messages from the validator
+        // get the error messages from the validator if any
 	if ($validator->fails()) {
             $errmsgs = $validator->messages();
+
 	} else {
+
+	    // calculate the bill per person
             if (($request->has('act') and $request->input('act') == 'calculate')) {
+
+	        // storage array for how much to tip depending on the quality of service
+        	static $serviceType = [                             
+   		      "poor" => 0.10,
+		      "good" => 0.15,
+		      "great"=> 0.20,
+	  	    ];
+
 	        $service = $serviceType[$tip];  
 
 	        // calculate bill per person with tip
@@ -68,7 +71,7 @@ class BillSplitterController extends Controller
 	        // format bill per person in USD notation
                 $ppBill = number_format($ppBill, 2, ".", "");
 	    }
-	}
+        }
 
         return view('index')->with([
 	     'totalBill' => $totalBill,
